3dd61ee7a650f09bb495829aca982e83e0dda6ab
Bambi <milan.t.kadri@gmail.com>
2025-02-13 17:35:31 +0100

    sum dropdown changes be guuut

--- app/src/main/java/de/syntax_institut/jetpack/a04_05_online_shopper/ui/views/HomeView.kt
+++ app/src/main/java/de/syntax_institut/jetpack/a04_05_online_shopper/ui/views/HomeView.kt
@@ -31,7 +31,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.unit.dp
 import de.syntax_institut.jetpack.a04_05_online_shopper.R
 import de.syntax_institut.jetpack.a04_05_online_shopper.data.viewmodels.ViewModelApi
